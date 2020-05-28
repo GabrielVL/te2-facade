ac4bca0b11b18250344cf3ac10c80be635735111
GabrielVL <gavalo.99@gmail.com>
2020-05-27 18:04:08 -0600

    [Clase Cliente] Se crea el cliente, donde se correrá el programa

---
+++ src/com/ce/datosi/Cliente.java
@@ -0,0 +1,13 @@
+package com.ce.datosi;
+
+import com.ce.datosi.pulperia.Cajero;
+
+//Main
+public class Cliente {
+
+    public static void main(String[] args) {
+        Cajero cajero = new Cajero();
+        cajero.atenderCliente();
+    }
+
+}
